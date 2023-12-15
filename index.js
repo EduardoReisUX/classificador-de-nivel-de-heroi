a4a66d29cf2267e52f2ccd77bca3bb895091406a
Eduardo dos Reis <ducabruno09@hotmail.com>
2023-12-15 14:00:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,44 @@
-"use strict"
+'use strict';
+
+const niveis_disponiveis = {
+  1000: 'Ferro',
+  2000: "Bronze",
+  5000: "Prata",
+  7000: "Ouro",
+  8000: "Platina",
+  9000: "Ascendente",
+  10000: "Imortal"
+}
 
 /**
- * 
+ *
  * @param {number} nivel_heroi nível do herói
  * @returns {string}
  */
 function calcularNivel(nivel_heroi) {
-    if (typeof nivel_heroi !== "number") {
-        throw new Error("nivel_heroi não é um número")
-    }
+  if (typeof nivel_heroi !== 'number') {
+    return 'nivel_heroi não é um número'
+  }
 
-    if (nivel_heroi < 1000) {
-        return "Ferro"
-    }
-    if (nivel_heroi < 2000) {
-        return "Ferro"
-    }
-    if (nivel_heroi < 5000) {
-        return "Ferro"
-    }
-    if (nivel_heroi < 7000) {
-        return "Ferro"
-    }
-    if (nivel_heroi < 8000) {
-        return "Ferro"
-    }
-    if (nivel_heroi < 9000) {
-        return "Ferro"
-    }
-    if (nivel_heroi < 10000) {
-        return "Ferro"
-    }
-    
-    return "Radiante"
+  for (const nivel in niveis_disponiveis) {
+    if (nivel_heroi < nivel)
+      return niveis_disponiveis[nivel]
+  }
+
+  return "Radiante"
 }
 
 /**
- * 
+ *
  * @param nome {string} Nome do herói
  * @param nivel {string} Nível do herói
  * @returns {string}
  */
-function render(nome, nivel) {
-    return `O Herói de nome ${nome} está no nível ${nivel}`
+function exibir(nome, nivel) {
+  return `O Herói de nome ${nome} está no nível ${nivel}`;
 }
 
-const nome = "fulano"
-const nivel = 3456
-
-const elo = calcularNivel(nivel)
-
-
-console.log(render(nome, elo))
+let nome = 'fulano';
+let nivel = 10456;
+let elo = calcularNivel(nivel);
+console.log(exibir(nome, elo));
